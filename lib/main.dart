cf278ca491b6eebe5669777cab5d8ff068c7f5b2
Leonardo Hermes <leohermescarvalho18@gmail.com>
2024-04-23 21:33:39 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,22 @@ import 'package:appaviao/pages/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: const login(),
-        appBar: AppBar(
-            title: const Text("Teste tela"),
-            backgroundColor: const Color.fromARGB(255, 20, 20, 204)),
+      debugShowCheckedModeBanner: false,
+      title: 'Login Page',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const login(),
     );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class login extends StatelessWidget {
@@ -5,20 +6,95 @@ class login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Card(
-          color: Color.fromARGB(255, 32, 28, 169),
-          child: ListTile(
-            leading: Icon(Icons.music_note),
-            title: Text("System Of Down"),
-            subtitle: Text("cantam: B.O.B"),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          title:
+              const Image(image: AssetImage("lib/assets/images/dogdino.png"))),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(27),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurple,
+              Colors.pinkAccent,
+            ],
           ),
         ),
-        ListTile(
-          key: Key('piroca'),
-        )
-      ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            const Text(
+              "Digite os dados de acesso nos campos abaixo.",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const CupertinoTextField(
+              cursorColor: Colors.pinkAccent,
+              padding: EdgeInsets.all(15),
+              placeholder: "Digite o seu e-mail",
+              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
+                  )),
+            ),
+            const SizedBox(height: 5),
+            const CupertinoTextField(
+              padding: EdgeInsets.all(15),
+              cursorColor: Colors.pinkAccent,
+              placeholder: "Digite sua senha",
+              obscureText: true,
+              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
+                  )),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: CupertinoButton(
+                padding: const EdgeInsets.all(17),
+                color: Colors.greenAccent,
+                child: const Text(
+                  "Acessar",
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(height: 7),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white70, width: 0.8),
+                  borderRadius: BorderRadius.circular(7)),
+              child: CupertinoButton(
+                child: const Text(
+                  "Crie sua conta",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
