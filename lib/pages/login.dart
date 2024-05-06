299a28f1524c71d114051a163b5bc3d806297043
ErickSouza18 <erick.cesar.lopes.souza@gmail.com>
2024-05-06 20:14:18 -0300

    Tela criar conta

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,5 @@
-import 'package:appaviao/pages/tela_criar_conta.dart';
 import 'package:appaviao/Custons/Custom_tela_login/custom_from_login.dart';
+import 'package:appaviao/pages/tela_criar_conta.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:appaviao/pages/tela_inicial.dart';
@@ -40,84 +40,92 @@ class login extends StatelessWidget {
               ),
             ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            const Text(
-              "Digite suas Credenciais para o acesso.",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 30),
-            const CupertinoTextField(
-              cursorColor: Colors.pinkAccent,
-              padding: EdgeInsets.all(15),
-              placeholder: "Digite o seu e-mail",
-              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(7),
-                  )),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(15),
-              cursorColor: Colors.pinkAccent,
-              placeholder: "Digite sua senha",
-              obscureText: true,
-              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(7),
-                  )),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              child: CupertinoButton(
-                padding: const EdgeInsets.all(17),
-                color: const Color.fromARGB(255, 69, 159, 227),
-                child: const Text(
-                  "Acessar",
-                  style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
-                onPressed: () {},
-              ),
-            ),
-            const SizedBox(height: 7),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white70, width: 0.8),
-                  borderRadius: BorderRadius.circular(7)),
-              child: CupertinoButton(
-                child: const Text(
-                  "Crie sua conta",
-                  style: TextStyle(
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(27),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 30),
+                  const SizedBox(
+                    height: 330,
+                  ), //espaço entre os campos
+                  const Text(
+                    "LOGIN.",
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const criarconta()),
-                  );
-                },
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  custom_from_login(
+                    labelText: "Nome",
+                    onSaved: (value) {
+                      // handle save
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira o seu e-mail';
+                      }
+                      return null;
+                    },
+                    icon: const Icon(Icons.email),
+                  ),
+                  const SizedBox(height: 10),
+                  custom_from_login(
+                    labelText: "Digite sua senha",
+                    onSaved: (value) {
+                      // handle save
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira a sua senha';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 30),
+                  CupertinoButton(
+                    color: const Color.fromARGB(255, 69, 159, 227),
+                    child: const Text("Acessar"),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const tela_inicial()));
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 7),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white70, width: 0.8),
+                        borderRadius: BorderRadius.circular(7)),
+                    child: CupertinoButton(
+                      child: const Text(
+                        "Crie sua conta",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const criarconta()),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
