adc64b307defc6680f99cb80ceafdfe9de2c8477
ErickSouza18 <erick.cesar.lopes.souza@gmail.com>
2024-05-06 20:28:03 -0300

    Tela Criar Conta botão azul

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:appaviao/pages/tela_cadastro_trecho.dart';  
+import 'package:appaviao/pages/login.dart';
+import 'package:appaviao/pages/tela_cadastro_trecho.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,6 +18,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const tela_cadastro_trecho());
+        home: login());
   }
 }
