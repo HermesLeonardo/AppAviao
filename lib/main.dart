f17c107be481b084583936ddbddbf47abbd48a9f
Leonardo Hermes <leohermescarvalho18@gmail.com>
2024-05-28 21:47:23 -0300

    commit de gerenciamento

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const tela_cadastro_aeroporto());
+        home: const tela_inicial());
   }
 }
